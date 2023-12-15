99d98631143514ba521fc5f0298633028223777f
Loiwibu2k4 <nguyenthangloi19052004@gmail.com>
2023-12-15 22:06:47 +0700

    nhung bootrap vs file static

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const route = require("./routes/client/index.route");
 app.set("views", "./views");
 app.set('view engine', "pug");
 
+app.use(express.static("public"));
+
 route(app);
 
 app.listen(port, () => {
