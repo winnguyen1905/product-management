45c5c83f29231d175d4821c8bf66f5c8d3c739d0
Loiwibu2k4 <nguyenthangloi19052004@gmail.com>
2023-12-16 12:18:46 +0700

    1.5 layout

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -3,5 +3,6 @@ const homeRoutes = require("./home.route");
 
 module.exports = (app) => {
     app.use("/", homeRoutes);
+    
     app.use("/products", productRoutes);
-}
+};
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["client/layouts/default.pug"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
