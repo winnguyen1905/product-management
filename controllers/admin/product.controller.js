ac9434a82c925ae93a4b216820df604c34915efb
Loiwibu2k4 <nguyenthangloi19052004@gmail.com>
2023-12-16 22:14:29 +0700

    1.8 output product in admin

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,5 +1,12 @@
+// [GET] /admin/products
+
+const Product = require("../../models/product.model");
+
 module.exports.index = async (req, res) => {
+    const products = await Product.find({});
+    console.log(products);
     res.render("admin/pages/products/index", {
-        pageTitle : "Product Admin"
+        pageTitle : "Product Admin",
+        products : products
     });
 };
