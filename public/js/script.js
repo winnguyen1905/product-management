b8726674c27dc2fd9c531bad4bef3df22f42f4da
Loiwibu2k4 <nguyenthangloi19052004@gmail.com>
2023-12-17 17:31:03 +0700

    2.1 search in FE

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,5 @@
+
+// Button status
 const buttonStatus = document.querySelectorAll("[button-status");
 console.log(buttonStatus);
 if(buttonStatus.length > 0) {
@@ -6,7 +8,7 @@ if(buttonStatus.length > 0) {
 
     buttonStatus.forEach((button) => {
         button.addEventListener("click", (event) => {
-            
+
             const status = button.getAttribute("button-status");
             console.log(status);
             if(status) {
@@ -18,3 +20,18 @@ if(buttonStatus.length > 0) {
         });
     });
 };
+
+// Form search
+const formSearch = document.querySelector("#form-search");
+if(formSearch) {
+    formSearch.addEventListener("submit", (event) => {
+        let url = new URL(window.location.href);
+        const keyword = event.target.elements.keyword.value;
+        if(keyword) {
+            url.searchParams.set("keyword", keyword);
+            window.location.href = url.href;
+        } else url.searchParams.delete("keyword");
+        event.preventDefault();
+        // console.log();
+    })
+}
